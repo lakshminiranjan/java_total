01dfc687d4a3a1cfeae6f8d34902c300b824161b
Lakshmi Niranjan <88361371+lakshminiranjan@users.noreply.github.com>
2022-06-14 17:53:47 +0600

    Add files via upload

---
+++ Java_code/Helloworld.java
@@ -0,0 +1,11 @@
+/*
+ * 
+ * 
+ * class and psvm is imp
+ * For everything they are common
+ */
+public class Helloworld{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
